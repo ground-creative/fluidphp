ea1a35ecce87d15da35304f6096729ba45fac28f
ifsale <carlo@salapc.com>
2014-11-18 15:33:24 +0100

    removed example routes

--- src/app/routes.php
+++ src/app/routes.php
@@ -8,17 +8,3 @@
 	| Use this file to register all of the routes for your application.
 	|
 	*/
-	
-	Router::group( 'main' , function( )
-	{
-		Router::get( '/' , function( )
-		{
-			return "the index page";
-		} );
-		
-	} )->prefix( App::option( 'app.env' ) );
-
-	Router::notFound( 404 , function( ) // not found urls
-	{
-		return "sorry no route was found";
-	} );
